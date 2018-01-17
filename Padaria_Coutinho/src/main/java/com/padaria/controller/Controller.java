669688d35a403fe9ebe876fa650a419884e13e34
ArtCouSan <artcousan@gmail.com>
2018-01-17 00:01:22 -0200

    validações

--- Padaria_Coutinho/src/main/java/com/padaria/controller/Controller.java
+++ Padaria_Coutinho/src/main/java/com/padaria/controller/Controller.java
@@ -1,83 +1,189 @@
 package com.padaria.controller;
 
 import java.math.BigDecimal;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.*;
 import com.jfoenix.validation.RequiredFieldValidator;
 import com.padaria.dao.ProdutoDAO;
 import com.padaria.model.Produto;
+import com.padaria.validator.Validation;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
-public class Controller{
-	
+public class Controller implements Initializable {
+
 	// Tela Cadastro
 	@FXML
 	private JFXTextField campoCodigoBarra;
-	
+
 	@FXML
-	private JFXComboBox<?> comboCategoria;
-	
+	private JFXComboBox<String> comboCategoria;
+
 	@FXML
-	private JFXComboBox<?> comboMarca;
-	
+	private JFXComboBox<String> comboMarca;
+
 	@FXML
 	private JFXTextField campoNome;
-	
+
 	@FXML
 	private JFXTextArea campoDescricao;
-	
+
 	@FXML
 	private JFXTextField campoDtVencimento;
-	
+
 	@FXML
 	private JFXTextField campoQtnComprada;
-	
+
 	@FXML
 	private JFXTextField campoQtnEstoque;
-	
+
 	@FXML
 	private JFXTextField campoPreco;
-	
+
 	@FXML
-	private JFXButton botaoCadastrar;
-	
-	
+	private Button botaoCadastrar;
+
 	@FXML
 	private Label labelNome;
-	
+
 	private static ProdutoDAO produtoDAO = new ProdutoDAO();
-	
+
+	// Botao que add produto
 	public void botaoAddProduto() {
-	
-		RequiredFieldValidator validator = new RequiredFieldValidator();
 		
-//		campoCodigoBarra.getV
-				
+		// Validação
+		Validation validator = new Validation();
+		
 		Produto produto = new Produto();
 		
-		produto.setCodigoBarra(campoCodigoBarra.getText());
-
-		produto.setNome(campoNome.getText());
+		String codigoBarra = null, 
+				nome = null, 
+				categoria = null, 
+				marca = null, 
+				descricao = null, 
+				dtVencimento = null, 
+				qtnComprada = null, 
+				qtnPreco = null;
 		
-		produto.setDescricao(campoDescricao.getText());
+		// Validações de regra de negocio do codigo de barra
+		if(validator.validationField(campoCodigoBarra)) {
+			
+			
+		}else {
+			
+			codigoBarra = campoCodigoBarra.getText();
+			
+		}
 		
-		produto.setDtVencimento(campoDtVencimento.getText());
+		// Validações de regra de negocio do nome
+		if(validator.validationField(campoNome)) {
+			
+		}else {
+			
+			nome = campoNome.getText();
+			
+		}
 		
-		produto.setQtnComprada(new Integer(campoQtnComprada.getText())); 
+		// Validações de regra de negocio de data de vencimento
+		if(validator.validationField(campoDtVencimento)) {
+			
+		}else {
+			
+			dtVencimento = campoDtVencimento.getText();
+			
+		}
 		
-		produto.setQtnEstoque(new Integer(campoQtnEstoque.getText()));
+		// Validações de regra de negocio da quantidade comprada
+		if(validator.validationField(campoQtnComprada)) {
+			
+		}else {
+			
+			qtnComprada = campoQtnComprada.getText(); 
+			
+		}
 		
-		produto.setPreco(new BigDecimal(campoPreco.getText()));
+		// Validações de regra de negocio do preço
+		if(validator.validationField(campoPreco)) {
+			
+		}else {
+			
+			qtnPreco = campoPreco.getText();
+			
+		}
 		
+		// Validações de regra de negocio da descrição
+		if(validator.validationArea(campoDescricao)) {
+			
+		}else {
+			
+			descricao = campoDescricao.getText();
+			
+		}
+		
+		// Validações de regra de negocio da marca
+		if(validator.validationCombo(comboMarca)) {
+					
+		}else {
+					
+			marca = comboMarca.getValue();
+					
+		}
+				
+		// Validações de regra de negocio da categoria
+		if(validator.validationCombo(comboCategoria)) {
+					
+		}else {
+					
+			categoria = comboCategoria.getValue();
+					
+		}
+		
+		produto.setCodigoBarra(codigoBarra);
+				
+		produto.setNome(nome);
+		
+		produto.setCategoria(categoria);
+		
+		produto.setMarca(marca);
+		
+		produto.setDescricao(descricao);
+		
+		produto.setDtVencimento(dtVencimento);
+		
+		produto.setQtnComprada(new Integer(qtnComprada)); 
+				
+		produto.setPreco(new BigDecimal(qtnPreco));
+		
+		// Insere produto no banco de dados
 		produtoDAO.addProduto(produto);
 		
+	}
+
+	// Inicial
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		RequiredFieldValidator validator = new RequiredFieldValidator();
+
+		campoCodigoBarra.getValidators().add(validator);
+
+		campoCodigoBarra.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if (!newValue) {
+					campoCodigoBarra.validate();
+				}
+			}
+		});
+
 	}
 
 }
